71cb5cba3cca7f51dab5046d58732c7f92501c6a
Rafael Rodrigues <rafaelleite701@outlook.com>
2024-10-13 14:00:16 -0300

    Atualizando código

--- eclipse-workspace/DesafioJavaFormulário/src/entities/Order.java
+++ eclipse-workspace/DesafioJavaFormulário/src/entities/Order.java
@@ -1,5 +1,47 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import enums.OrderStatus;
+
 public class Order {
-	//testando1
+	private Date moment;
+	private OrderStatus status;
+	private List<OrderItem> item = new ArrayList<>();
+	public Order() {
+	}
+	public Order(Date moment,OrderStatus status) {
+		this.moment= moment;
+		this.status= status;
+	}
+	public Date getMoment() {
+		return moment;
+	}
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	public List<OrderItem> getLis() {
+		return item;
+	}
+	
+	
+	public void addItem(OrderItem item) {
+		this.item.add(item);
+	}
+	public void removeItem(OrderItem item) {
+		this.item.remove(item);
+	}
+	
+	
 }
+	
+
